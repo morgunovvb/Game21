55604d3c4de2de6f8120b45c4681c9b49a72b5af
morgunovvb <74514540+morgunovvb@users.noreply.github.com>
2021-10-12 08:05:32 +0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -3,3 +3,8 @@ random.shuffle(koloda)
 koloda = [6,7,8,9,10,2,3,4,11] * 4
 print('Lets play?') 
 count = 0
+while True: 
+  choice = input('Take one card? y/n' ) 
+  if choice == 'y': 
+  current = koloda.pop() 
+  print('Your card is %d' %current)
